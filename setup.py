981e499b88ba7561515df782c0237200defd64f7
luisffee <luizffeelix@gmail.com>
2024-11-22 11:00:53 -0300

    :wrench: refactor: Started refactoring some files and launchs

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import find_packages, setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'fbot_speech'
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['butia_speech'],
-    package_dir={'': 'src'})
-
-setup(**setup_args)
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='stihl-bolsistas',
+    maintainer_email='stihl-bolsistas@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
